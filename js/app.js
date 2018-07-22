cb3875e24f9dd76ef3523ff61a2b49775a4fb946
zebrilee <zebrilee@gmail.com>
2018-07-22 22:56:43 +0200

    feat: set timer and stars, add modal - wip

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
-/*
- * Create a list that holds all of your cards
- */
-
  // declaration of all the variables we need
 const cards = [
     "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb",
     "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"
 ],
-deck = document.querySelector('#deck');
+deck  = document.querySelector('#deck'),
+stars = document.querySelectorAll('.stars li'),
+clock = document.querySelector('.clock');
 
 let matchingCards = [],
-pairNumberReached = 0,
-moves = 0;
+    pairNumberReached = 0,
+    moves = 0,
+    clockDelay,
+    time = 0,
+    timeLapse,
+    timerOff = true;
 
+    let seconds = 0,
+    minutes = 0,
+    hours = 0;
 
 // let's shuffle the cards before creating the grid!
 shuffle(cards);
@@ -31,7 +36,7 @@ shuffle(cards);
         li.append(span);
         ul.append(li);
     });
-})(); // we run the function, one other solution would have been to write createHtmlGrid(); on next line instead
+})();
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -53,48 +58,42 @@ function shuffle(array) {
 
 // card event listener function 
 addCardEventListener = () => {
+    const deck = document.querySelector('#deck');
+
     deck.addEventListener('click', event => {
-       const clickedCard = event.target;
-       if (isClickValid){
-           turnedOverCard(clickedCard);
-           cardToCompare(clickedCard);
-           if (matchingCards.length === 2){
-               // here we need to check if card are matching
-               matchCard(clickedCard);
-               addCountMove();
-           }
+        const clickedCard = event.target;
+
+
+        if (timerOff) {
+            timerOff = false;
+            startTimer();
         }
-        
-        });
+
+        displayCardSymbol(clickedCard);
+        matchingCards.push(clickedCard); // array was empty first
+        if (matchingCards.length === 2) {
+            // here we need to check if card are matching
+            matchCard(clickedCard);
+            addCountMove();
+            starRating();
+        }
+   
+    });
 }
 addCardEventListener();
 
 
 
 // add class to card that are turned over
-turnedOverCard = (card) => {
+displayCardSymbol = (card) => {
     card.classList.toggle('open');
     card.classList.toggle('show');
 }
 
-// Add turned over card in an array to compare them
-cardToCompare = (clickedCard) => {
-    matchingCards.push(clickedCard);
-}
-
-isClickValid = (clickTarget) => {
-    return (
-        clickTarget.classList.contains('card') &&
-        !clickTarget.classList.contains('match') &&
-        toggledCards.length < 2 &&
-        !toggledCards.includes(clickTarget)
-    );
-}
-
-
 // check if two cards match function
 matchCard = () => {
-    const pairOfCard = 8;
+    const pairOfCard = 4;
+    
     
     if (matchingCards[0].firstChild.className === 
         matchingCards[1].firstChild.className) {
@@ -103,27 +102,154 @@ matchCard = () => {
         matchingCards = [];
         pairNumberReached++;
         if (pairNumberReached === pairOfCard) {
-            //gameOver();
+            gameOver();
             console.log('the end');
+
+            
         }
     } else{
         setTimeout(() => {
-        turnedOverCard(matchingCards[0]);
-        turnedOverCard(matchingCards[1]);
+        displayCardSymbol(matchingCards[0]);
+        displayCardSymbol(matchingCards[1]);
         matchingCards = [];
         }, 1000);
         
     };
 }
 
-// gameOver = () => {
-//     console.log('the end');
-// }
-
+// count the number of moves function
 addCountMove = () => {
     moves++;
-    const movesText = document.querySelector('.moves');
-    movesText.innerHTML = moves;
+    const movesNumber = document.querySelector('.moves');
+    movesNumber.innerHTML = moves;
+}
+
+// timer function 
+startTimer = () => {
+    const timer = document.querySelector('.clock');
+
+    clockDelay = setInterval(function () {
+        if (seconds < 10) {
+            timer.innerHTML = `${minutes}:0${seconds}`;
+        } else {
+            timer.innerHTML = `${minutes}:${seconds}`;
+        }
+        seconds++;
+        if (seconds == 60) {
+            minutes++;
+            seconds = 0;
+        }
+        if (minutes == 60) {
+            hours++;
+            minutes = 0;
+        }
+
+    }, 1000);
+}
+
+setStars = () => {
+
+    let starRating = 3;
+    if (moves <= 8) {
+        starRating = 3;
+    } else if (moves > 8 && moves <= 15) {
+        starRating = 2;
+    } else {
+        starRating = 1;
+    }
+}
+
+starRating = () =>{
+    if (moves === 6 || moves === 8) {
+        setStars();
+    }
+}
+
+stopTimer = () => {
+    clearInterval(clockDelay);
+    timeLapse = timer.innerHTML;
+}
+
+gameOver = () => {
+    console.log('the end');
+    const modal = document.querySelector('.overlay');
+    modal.classList.add('show-popup');
+    //stopTimer();
+    messageScore();
+}
+
+// create Modal 
+createHtmlModal = () => {
+    const container = document.querySelector('.container');
+    const modal = document.createElement('div');
+    modal.className = "overlay";
+    
+    const popup = document.createElement('div');
+    popup.className = "popup";
+    modal.append(popup);
+
+    const h2 = document.createElement('h2');
+    h2.innerHTML = "Congratulations !!! You rocks!";
+    popup.append(h2);
+
+    const h3 = document.createElement('h3');
+    h3.innerHTML = "Stats of the game";
+    popup.append(h3);
+
+    const resultStats = document.createElement('div');
+    resultStats.className = "result-score";
+    popup.append(resultStats);
+
+    const movesNumber = document.createElement('p');
+    movesNumber.className = 'movesNumber';
+    resultStats.append(movesNumber);
+
+    const timeDuration = document.createElement('p');
+    timeDuration.className = ' timeDuration';
+    resultStats.append(timeDuration);
+
+    const starRating = document.createElement('p');
+    starRating.className = 'starRating';
+    resultStats.append(starRating);
+
+    const button = document.createElement('button');
+    button.innerHTML = 'New game'
+    button.className = 'new-game';
+    popup.append(button);
+
+    container.append(modal);
+    
+};
+createHtmlModal();
+
+
+messageScore = () => {
+    
+    //showing move, rating, time on modal
+    document.querySelector(".movesNumber").innerHTML = 'Moves : ' + moves;
+    document.querySelector(".starRating").innerHTML = 'Stars : ' + starRating;
+    document.querySelector(".timeDuration").innerHTML = 'Time :  ' + timeDuration;
+}
+
+resetMoves = () => {
+    moves = 0;
+    document.querySelector('.moves').innerHTML = moves;
+}
+
+
+resetGame = () => {
+    //resetClockAndTime();
+    resetMoves();
+    resetStars();
+    // resetCards();
+    shuffle(cards);
+}
+document.querySelector('.restart').addEventListener('click', resetGame);
+
+closeModal = () =>{
+    resetGame();
+    modal = document.querySelector('.overlay');
+    modal.classList.remove("show-popup");
 }
 
 /*
